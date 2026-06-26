56f5a0686bbc6ab73eca25139abe3d32781ba266
Алексей <METEOP2024@yandex.ru>
2026-06-26 14:25:48 +0300

    Исправлен файл FieldLayout.js удален файл FieldLayout.module.css/ Изменение React + Tailwind

--- my-app/src/Game/Components/Field/FieldLayout.js
+++ my-app/src/Game/Components/Field/FieldLayout.js
@@ -1,17 +1,16 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { PLAYER, PLAYER_SIGN } from '../../constants';
-import styles from './FieldLayout.module.css';
 export class FieldLayout extends Component {
 	render() {
 		const { field, handleCellClick } = this.props;
 
 		return (
-			<div className={styles.field}>
-				{ field.map((cellPlayer, index) => (
+			<div className="grid grid-cols-[repeat(3,80px)] grid-rows-[repeat(3,80px)] gap-2">
+				{field.map((cellPlayer, index) => (
 					<button
 						key={index}
-						className={styles.cell}
+						className="text-2xl font-bold cursor-pointer bg-gray-200 border-2 border-black rounded-md transition-colors duration-200 ease-in-out disabled:cursor-default disabled:bg-gray-300 disabled:text-gray-500"
 						onClick={() => handleCellClick(index)}
 						disabled={cellPlayer !== PLAYER.NOBODY}
 						type="button"
@@ -27,4 +26,4 @@ export class FieldLayout extends Component {
 FieldLayout.propTypes = {
 	field: PropTypes.arrayOf(PropTypes.oneOf(Object.values(PLAYER))),
 	handleCellClick: PropTypes.func,
-}
+};
